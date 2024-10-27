4b8a8469847dfcd9e984ec2c1852b425a00ab7ac
blackicon-eth <mattiaverdecchi@yahoo.it>
2024-10-28 00:44:01 +0100

    cleanup

--- src/routes/utils/metrics.ts
+++ src/routes/utils/metrics.ts
@@ -1,38 +1,11 @@
 import { Request, Response } from "express";
-// import { castsQueue } from "../../queues/casts.js";
 import { dcsQueue } from "../../queues/dcs.js";
 import { xmtpQueue } from "../../queues/xmtp.js";
 import { repliesQueue } from "../../queues/replies.js";
 
 export const metricsHandler = async (_: Request, res: Response) => {
-  // const [
-  //   // castsFailed,
-  //   repliesFailed,
-  //   repliesCompleted,
-  //   dcsFailed,
-  //   dcsCompleted,
-  //   xmtpFailed,
-  //   xmtpCompleted,
-  // ] = await Promise.all([
-  //   // castsQueue?.getMetrics("failed"),
-  //   repliesQueue?.getMetrics("failed"),
-  //   repliesQueue?.getMetrics("completed"),
-  //   dcsQueue?.getMetrics("failed"),
-  //   dcsQueue?.getMetrics("completed"),
-  //   xmtpQueue?.getMetrics("failed"),
-  //   xmtpQueue?.getMetrics("completed"),
-  // ]);
-
   res.status(200).send({
     result: {
-      // casts: castsFailed
-      //   ? {
-      //       completed: castsFailed!.meta.count - castsFailed!.count,
-      //       failed: castsFailed?.count,
-      //       performance:
-      //         100 - (castsFailed!.count / castsFailed!.meta.count) * 100,
-      //     }
-      //   : null,
       replies: {
         completed: await repliesQueue?.getCompletedCount(),
         failed: await repliesQueue?.getFailedCount(),
