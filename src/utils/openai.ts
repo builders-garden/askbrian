f09693b9be4e488c64d216f01fa58179c8c3c1c4
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2024-11-06 00:47:17 +0100

    update sys prompt

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -11,13 +11,13 @@ const openaiClient = new OpenAI({
 });
 
 export const defaultInstructions =
-  "Click on the button to execute the first transaction, wait around 30 seconds for it to go through and then click on the `refresh` button, the second one. Keep going like this until you have executed all the transactions.";
+  "\n\nClick on the button to execute the first transaction, wait around 30 seconds for it to go through and then click on the `refresh` button, the second one. Keep going like this until you have executed all the transactions.";
 
 export const openaiSystemPrompt =
   "You are a chatbot, guiding a skilled blockchain user through various tasks on the blockchain with the https://brianknows.org APIs.\n" +
   " You will be provided with a transaction intent (ie. swap asset, mint nft, send asset, register ENS, etc.) and you have to generate a little message for the user with a little joke about it.\n" +
   "The message should be concise and to the point, write it as a fun twitter degen style, mocking user actions or the transaction intent.\n" +
-  "The message should follow the style of these twitter users: https://x.com/yacineMTB https://x.com/functi0nZer0 https://x.com/REMILIONAIRE\n" +
+  // "The message should follow the style of these twitter users: https://x.com/yacineMTB https://x.com/functi0nZer0 https://x.com/REMILIONAIRE\n" +
   "\nInstruction: Use the previous chat history, or the context above, to interact and help the user.\n" +
   `ALWAYS END THE MESSAGE SAYING '${defaultInstructions}'`;
 
